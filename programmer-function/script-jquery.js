e00ea3fbc61b9a5e4caa2d42029d3f3b0e19e560
Michael Bowen <mikee.bowen@gmail.com>
2015-03-20 12:34:24 -0700

    add latest

--- programmer-function/script-jquery.js
+++ programmer-function/script-jquery.js
@@ -1,3 +1,37 @@
-$(document).ready(function() {
+$(document).ready(function(){
+  var programmersArray = [ { name: "Bob", occupation: "programmer", awesomeIndex: 7, }, { name: "Alice", ccupation: "programmer", awesomeIndex: 9, }, { name: "Zaphod", occupation: "President of the Galaxy" } ];
+  // $("#jquery-button").on("click", function () {
+  //   var avAwesome = 0;
+  //   for (var i = 0; i < programmersArray.length; i++) {
+  //     if (programmersArray[i]["awesomeIndex"]) {
+  //       avAwesome += programmersArray[i]["awesomeIndex"];
+  //       avAwesome = avAwesome/(i + 1);
+  //     }
+  //   }
+  //   $("#awesome-index").html(avAwesome);
+  // })
+  // $("#jquery-button").on("click", function () {
+  //   var test = _.sum([1,2,3,4]);
+  // console.log(test);
+  // })
 
-}
+// var findAvgAwesome = function (array) {
+//    var avAwesome = 0;
+//     for (var i = 0; i < array.length; i++) {
+//       if (array[i]["awesomeIndex"]) {
+//         avAwesome += array[i]["awesomeIndex"];
+//         avAwesome = avAwesome/(i + 1);
+//       }
+//     }
+//     return avAwesome;
+//   };
+// _.each(programmersArray, findAvgAwesome);
+// var evens = _.sum([4, 6, 2]);
+// console.log(evens);
+  //   var test = _.sum([1,2,3,4]);
+  // console.log(test);
+
+  var test2 = _([1, 2]).forEach(function(n) {
+  console.log(n);
+}).value();
+});
