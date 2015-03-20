09f7bfc9421b49729acb1eff5aaf7a9e333cb6b4
Michael Bowen <mikee.bowen@gmail.com>
2015-03-20 12:36:20 -0700

    uncomment working jquery

--- programmer-function/script-jquery.js
+++ programmer-function/script-jquery.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
   var programmersArray = [ { name: "Bob", occupation: "programmer", awesomeIndex: 7, }, { name: "Alice", ccupation: "programmer", awesomeIndex: 9, }, { name: "Zaphod", occupation: "President of the Galaxy" } ];
-  // $("#jquery-button").on("click", function () {
-  //   var avAwesome = 0;
-  //   for (var i = 0; i < programmersArray.length; i++) {
-  //     if (programmersArray[i]["awesomeIndex"]) {
-  //       avAwesome += programmersArray[i]["awesomeIndex"];
-  //       avAwesome = avAwesome/(i + 1);
-  //     }
-  //   }
-  //   $("#awesome-index").html(avAwesome);
-  // })
-  // $("#jquery-button").on("click", function () {
-  //   var test = _.sum([1,2,3,4]);
-  // console.log(test);
-  // })
+  $("#jquery-button").on("click", function () {
+    var avAwesome = 0;
+    for (var i = 0; i < programmersArray.length; i++) {
+      if (programmersArray[i]["awesomeIndex"]) {
+        avAwesome += programmersArray[i]["awesomeIndex"];
+        avAwesome = avAwesome/(i + 1);
+      }
+    }
+    $("#awesome-index").html(avAwesome);
+  })
+  $("#jquery-button").on("click", function () {
+    var test = _.sum([1,2,3,4]);
+  console.log(test);
+  })
 
 // var findAvgAwesome = function (array) {
 //    var avAwesome = 0;
