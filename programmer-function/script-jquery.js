f196e3a25880b0cbbfd7e0e47e65e545ead14f8b
Michael Bowen <mikee.bowen@gmail.com>
2015-03-20 14:11:16 -0700

    make functions work when more objects are added to the array

--- programmer-function/script-jquery.js
+++ programmer-function/script-jquery.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
-  var programmersArray = [ { name: "Bob", occupation: "programmer", awesomeIndex: 7, }, { name: "Alice", ccupation: "programmer", awesomeIndex: 9, }, { name: "Zaphod", occupation: "President of the Galaxy" } ];
+  var programmersArray = [
+  { name: "Bob", occupation: "programmer", awesomeIndex: 7, },
+  { name: "Alice", ccupation: "programmer", awesomeIndex: 9, },
+  { name: "Zaphod", occupation: "President of the Galaxy" },
+  { name: "Alice", ccupation: "programmer", awesomeIndex: 5, },
+  { name: "Joe", ccupation: "programmer", awesomeIndex: 4, },
+  { name: "Bob", ccupation: "programmer", awesomeIndex: 10, },
+  { name: "July", ccupation: "programmer", awesomeIndex: 1, }
+  ];
+
   $("#jquery-button").on("click", function () {
     var avArray = [];
+    var divisor = 0;
     for (var i = 0; i < programmersArray.length; i++) {
       if (programmersArray[i]["awesomeIndex"]) {
-        avArray.push(programmersArray[i]["awesomeIndex"])
-        avAwesome = _.sum(avArray)/(i + 1);
+        divisor++;
+        avArray.push(programmersArray[i]["awesomeIndex"]);
+        avAwesome = _.sum(avArray)/(divisor);
       }
     }
     $("#awesome-index").html(avAwesome);
--- programmer-function/script.js
+++ programmer-function/script.js
@@ -1,16 +1,23 @@
 var programmers = [
 { name: "Bob", occupation: "programmer", awesomeIndex: 7, },
 { name: "Alice", ccupation: "programmer", awesomeIndex: 9, },
-{ name: "Zaphod", occupation: "President of the Galaxy" }
+{ name: "Zaphod", occupation: "President of the Galaxy" },
+{ name: "Alice", ccupation: "programmer", awesomeIndex: 5, },
+{ name: "Joe", ccupation: "programmer", awesomeIndex: 4, },
+{ name: "Bob", ccupation: "programmer", awesomeIndex: 10, },
+{ name: "July", ccupation: "programmer", awesomeIndex: 1, }
 ];
 
 var jsButton = document.getElementById("javascript-button");
 var javascriptFunction = function () {
+  var sumAverageAwesome = 0;
   var averageAwesome = 0;
+  var divisor = 0;
   for (var i = 0; i < programmers.length; i++) {
     if (programmers[i]["awesomeIndex"]){
-      averageAwesome += programmers[i]["awesomeIndex"];
-      averageAwesome = averageAwesome/(i + 1);
+      divisor++;
+      sumAverageAwesome += programmers[i]["awesomeIndex"];
+      averageAwesome = sumAverageAwesome/divisor;git s
     }
   }
 document.getElementById("awesome-index").innerHTML = averageAwesome;
